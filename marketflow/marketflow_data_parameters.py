fd7fd8a9da5b8e546395587fb49da79a51388bb8
Martinolli <joaoboscojbm@gmail.com>
2025-09-16 18:32:05 +0400

    Add weekly interval configuration to default timeframes

--- marketflow/marketflow_data_parameters.py
+++ marketflow/marketflow_data_parameters.py
@@ -305,6 +305,7 @@ default_config = {
     "pnf_scale": "percent",     # fixed | percent | atr
     "pnf_scale_value": 0.005,   # 0.5% default when using percent
     "timeframes": [
+        {"interval": "1w", "period": "1y"},
         {"interval": "1d", "period": "365d"},
         {"interval": "4h", "period": "100d"},
         {"interval": "1h", "period": "40d"},    
